b12fbb39161a6927c82e48b08a1f601afaee5f9f
snehasivadas21 <snehasivadas2002@gmail.com>
2026-08-19 22:47:09 +0530

    added vehicle and booking apis

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Vehicle,Booking
+from .serializers import VehicleSerializer,BookingSerializer
 
 # Create your views here.
+
+class VehicleViewSet(viewsets.ModelViewSet):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+
+    
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.select_related("vehicle").all()
+    serializer_class = BookingSerializer
+
+
+
--- vehicle_system/urls.py
+++ vehicle_system/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import (SpectacularAPIView,SpectacularSwaggerView)
+from inventory.views import VehicleViewSet,BookingViewSet
+
+router = DefaultRouter()
+
+router.register(r"vehicles",VehicleViewSet,basename="vehicle")
+router.register(r"bookings",BookingViewSet,basename="booking")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/',include(router.urls)),
+    
+    path('api/schema/',SpectacularAPIView.as_view(),name="schema"),
+    path('api/docs/',SpectacularSwaggerView.as_view(url_name="schema"),name="swagger-ui")
+
 ]
