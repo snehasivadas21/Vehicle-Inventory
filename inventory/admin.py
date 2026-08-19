e47daf1450d26b7d88a1d6f7336cfb4cb0d5b5c2
snehasivadas21 <snehasivadas2002@gmail.com>
2026-08-19 19:41:40 +0530

    added vehicle and booking models

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Vehicle,Booking
 
 # Register your models here.
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ("id","name","brand","year","price_per_day","fuel_type","is_available")
+    list_filter = ("brand","fuel_type","is_available")
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("id","vehicle","customer_name","customer_phone","start_date","end_date","total_amount")
+        
+
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Vehicle(models.Model):
+    class Fueltype(models.TextChoices):
+        PETROL = "Petrol","Petrol"
+        DIESEL = "Diesel","Diesel"
+        ELECTRIC = "Electric","Electric"
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    year = models.IntegerField()
+    price_per_day = models.DecimalField(max_digits=10,decimal_places=2)
+    fuel_type = models.CharField(max_length=20,choices=Fueltype.choices)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.brand} {self.name}"
+
+class Booking(models.Model):
+    vehicle = models.ForeignKey(Vehicle,on_delete=models.CASCADE,related_name="booking")
+    customer_name = models.CharField(max_length=100)
+    customer_phone = models.CharField(max_length=10)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    total_amount = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return f"{self.customer_name} - {self.vehicle}"
+
+
+
